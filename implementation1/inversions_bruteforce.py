2064dda2b6dbff5547f65566e3e672ce8fcab770
Trevor Bramwell <trevor.j.bramwell@gmail.com>
2013-01-30 12:03:12 -0800

    Add assertions to bruteforce

--- implementation1/inversions_bruteforce.py
+++ implementation1/inversions_bruteforce.py
@@ -7,20 +7,26 @@ Example: [1, 4, 2, 5, 3]
 
     There are 3 inversions: (4, 2), (4, 3), (2, 5)
     
+Note: This assumes the last element in the list is the total count of
+inversions
 """
 # Build a list of lists of ints
 lists = [[int(x) for x in line.split(',')] for line in open('sample_results').readlines()]
 
-a1 = [1,2,3,4,5]
-a2 = [1,4,2,5,3]
-a3 = [5,4,3,2,1]
+a1 = [1,2,3,4,5, 0]
+a2 = [1,4,2,5,3, 3]
+a3 = [5,4,3,2,1, 10]
 
 def main():
     # Append the sample lists
     [lists.append(a) for a in (a1,a2,a3)]
 
     # Print inversion counts for each list
-    print [inv_count(l) for l in lists]
+    counts = [(inv_count(l), l[len(l)-1]) for l in lists]
+    for i, j in counts:
+        assert i == j, "Inversion count off"
+
+    print counts
 
 
 def inv_count(lst):
@@ -28,7 +34,7 @@ def inv_count(lst):
     inv = []
     for i in range(0, len(lst)):
         # i+i ensures the element is not compared against itself
-        for j in range(i+1, len(lst)):
+        for j in range(i+1, len(lst)-1):
             if lst[i] > lst[j]:
                 inv.append((lst[i], lst[j]))
     return len(inv)
