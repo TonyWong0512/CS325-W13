384cfe1e3b91be6527d856689219bf5c5c92fcdb
Trevor Bramwell <bramwelt@osuosl.org>
2013-02-25 14:26:58 -0800

    IW2: Remove old test input file, updated pdf with new graph

--- implementation2/algorithms/tests2.py
+++ implementation2/algorithms/tests2.py
@@ -2,14 +2,16 @@
 """
 Maximum Subarray Tests
 """
-from brute_sum import brute_sum as algo
-#from divide_conquer import divide_conquer as algo
-#from dynamic import dynamic as algo
+from brute_sum import brute_sum
+from divide_conquer import divide_conquer
+from dynamic import dynamic
 
+algos = [brute_sum, divide_conquer, dynamic]
 # Build a list of lists of ints, each lists last element is the number
 # of sums in that list
 #lists = [[int(x) for x in line.split(',')] for line in open('test_input.txt').readlines()]
-lists = [[int(x) for x in line.split(',')] for line in open('test_input2.txt').readlines()]
+#lists = [[int(x) for x in line.split(',')] for line in open('test_input2.txt').readlines()]
+lists = [[int(x) for x in line.split(',')] for line in open('finaltest.txt').readlines()]
 
 def main():
     # Check sums
@@ -20,9 +22,13 @@ def main():
     print "Sums Correct"
 
 def create_sums():
-    for i in (algo(lst) for lst in lists):
-        print i
+    for algo in algos:
+        print algo
+        for i in (algo(lst) for lst in lists):
+            print i,
+            #print "%d \\\ \hline" % i
+        print
 
 if __name__ == "__main__":
-    main()
-    #create_sums()
+    #main()
+    create_sums()
