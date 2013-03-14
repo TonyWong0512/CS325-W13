7f13482d866efdef9b73c9bd9c808bc836d065cd
Trevor Bramwell <tehriddler@gmail.com>
2013-03-14 13:51:10 -0700

    IM3: Update kmeans file to use furthest node method. Remove old ones.

--- implementation3/tsp/kmeans.py
+++ implementation3/tsp/kmeans.py
@@ -13,49 +13,52 @@ does not change.
 """
 import math
 
+
+# XXX Current busted. Dicts are sets and keys will get overridden
+# Will work for some small k though...
+def furthest_nodes(nodes, k):
+    """
+    Given a set of nodes, and a k, compute k nodes with the maximum
+    distance from each other.
+
+    >>> max_points([(1,2),(3,4)], 2)
+    [(1, 2), (3, 4)]
+    >>> max_points([(0,0),(0,2),(2,2),(2,0)], 2)
+    [(0, 0), (2, 2)]
+    """
+    ks = []
+    ks.append(nodes[0])
+    # Ignore 1 k since we are starting with one
+    for i in range(k-1):
+        m_nodes = {}
+        for node in (n for n in nodes if n not in ks):
+            # key: distance, value: node
+            m_nodes[dist(ks[i], node)] = node
+
+        if m_nodes:
+            # m_nodes is not empty
+            max_key = max(m_nodes.keys())
+            # node with maximum distance away
+            ks.append(m_nodes[max_key])
+    return ks
+
 def dist(v1, v2):
-    """The euclidian distance between two vectors v1 and v2"""
+    """The euclidian distance between two vectors v1 and v2
+    >>> dist((0,0),(3,4))
+    5
+    """
     hypot = math.hypot(v1[0]-v2[0], v1[1]-v2[1])
     return int(round(hypot))
 
-def cluster_mean(cluster):
-    """The mean for a given cluster of nodes"""
-    x_m = y_m = 0.0
-    for node in cluster:
-        x_m += node[0]
-        y_m += node[1]
-    x_m /= len(nodes)
-    y_m /= len(nodes)
-    return (x_m, y_m)
-
-def kmeans(nodes, k):
-    means = []
-    # while ks != previous ks?, need to stop when they match
-
-    # Step 1, choose K as mean of each partition.
-    for i in range(k-1):
-        # if k = 3, this will assign ks[0] and ks[1]
-        means[i] = cluster_mean(nodes[:1/k])
-    # ks[2] will get the rest
-    means[k] = cluster_mean(nodes[1/k:])
-
-    # Compute distances for each mean
-    mins = []
-    for z in means:
-        # min 1/kth from tuple
-        mins[z] = (dist(means[z], nodes) for i in range(1, len(nodes))
-
-    #    print dist(nodes[0], nodes[x])
-    #print i
-
 def main():
-    nodes = [(1, 3), (3, 6), (5, 3), (2, 2)]
-    kmeans(nodes, 2)
-    #nodes2 = [(0,0),(0,1),(1,1),(1,0)]
-    #for x in range(1,len(nodes)):
-    #    print dist(nodes[0], nodes[x])
-    #print cluster_mean(nodes)
-    #print dist(nodes[0],nodes[1])
+    lists = [tuple([int(x) for x in line.split(' ')])[1:] for line in open('example-input-1.txt').readlines()]
+    print furthest_nodes(lists, 4)
+
+def test():
+    """Run doctests"""
+    import doctest
+    doctest.testmod()
 
 if __name__ == "__main__":
+    #test()
     main()
