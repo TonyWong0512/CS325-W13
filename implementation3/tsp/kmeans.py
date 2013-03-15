554bf7bdde110eccdc38b0d08a219d909fdaf509
Trevor Bramwell <tehriddler@gmail.com>
2013-03-15 01:36:04 -0700

    IM3: Rewrote kmeans for 3rd time.
    It's actually only a partial implementation of kmeans++. Removed all
    other failing algoritms.

--- implementation3/tsp/kmeans.py
+++ implementation3/tsp/kmeans.py
@@ -14,78 +14,62 @@ does not change.
 import math
 
 def dict_max(ds):
-    return max(d.iterkeys(), key=(lambda key: d[key]))
+    return max(ds.iterkeys(), key=(lambda key: ds[key]))
 
-# XXX Current busted. Dicts are sets and keys will get overridden
-# Will work for some small k though...
-def furthest_nodes2(nodes, k):
-    """
-    Given a set of nodes, and a k, compute k nodes with the maximum
-    distance from each other.
+def kmeans_pp(nodes, k):
+    """kmeans++
 
-    >>> max_points([(1,2),(3,4)], 2)
+    Doesn't actually compute kmeans, just does the setup of ++
+
+    Returns k central points
+
+    >>> kmeans_pp([(1,2),(3,4)], 2)
     [(1, 2), (3, 4)]
-    >>> max_points([(0,0),(0,2),(2,2),(2,0)], 2)
-    [(0, 0), (2, 2)]
+
+    >>> kmeans_pp([(0,0),(0,2),(2,2),(2,0)], 4)
+    [(0, 0), (2, 2), (0, 2), (2, 0)]
     """
-    k_nodes = []
-    k_nodes.append(nodes[0])
-    # Compute the maximum distance between k_nodes and nodes
-    for i in range(k-1):
-        m_nodes = {}
-        # Compute diances between k_nodes and nodes
-        for node in nodes:
-            k_max = {}
-            for k_n in k_nodes:
-                k_max[node] += dist(k_n, node)
+    assert(k >= 2)
+    assert(len(nodes) > 0)
 
-            if k_max:
-                maxs = dict_max(k_max)
-
-        if m_nodes:
-            # m_nodes is not empty
-            max_node = max(m_nodes.iterkeys(), 
-                key=(lambda key: m_nodes[key]))
-            ks.append(max_node)
-    return ks
-
-def furthest_nodes(nodes, k):
-    """
-    Given a set of nodes, and a k, compute k nodes with the maximum
-    distance from each other.
-
-    >>> max_points([(1,2),(3,4)], 2)
-    [(1, 2), (3, 4)]
-    >>> max_points([(0,0),(0,2),(2,2),(2,0)], 2)
-    [(0, 0), (2, 2)]
-    """
+    # Don't want to have any array bound errors
     ks = []
-    ks.append(nodes[0])
-    # Ignore 1 k since we are starting with one
-    for i in range(k-1):
-        m_nodes = {}
-        for node in (n for n in nodes if n not in ks):
-            # key: distance, value: node
-            m_nodes[node] = dist(ks[i], node)
 
-        if m_nodes:
-            # m_nodes is not empty
-            max_node = max(m_nodes.iterkeys(), 
-                key=(lambda key: m_nodes[key]))
-            ks.append(max_node)
+    # Choose the first center point
+    ks.append(nodes.pop(0))
+
+    # Find the farthest point from it
+    while len(ks) != k:
+        kmax = dist(ks[0], nodes[0])
+        max_node = nodes[0]
+
+        for node in (node for node in nodes if node not in ks):
+            possible = 0.0
+
+            for knode in ks:
+                possible += dist(knode, node)
+
+            if possible > kmax:
+                kmax = possible
+                max_node = node
+        ks.append(max_node)
+
     return ks
 
 def dist(v1, v2):
     """The euclidian distance between two vectors v1 and v2
     >>> dist((0,0),(3,4))
-    5
+    5.0
+    >>> dist((0,0),(9,40))
+    41.0
+    >>> dist((0,0),(5,12))
+    13.0
     """
-    hypot = math.hypot(v1[0]-v2[0], v1[1]-v2[1])
-    return int(round(hypot))
+    return math.hypot(v1[0]-v2[0], v1[1]-v2[1])
 
 def main():
-    lists = [tuple([int(x) for x in line.split(' ')])[1:] for line in open('example-input-1.txt').readlines()]
-    print furthest_nodes(lists, 4)
+    lists = [tuple([int(x) for x in line.split(' ')])[1:] for line in open('example-input-2.txt').readlines()]
+    print kmeans_pp(lists, 8)
 
 def test():
     """Run doctests"""
